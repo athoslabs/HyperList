5bb49c18232b6146f0733898a7cefd10b957e5d8
Robert Morrow <rcmorrow01@verizon.net>
2020-12-14 09:00:57 -0500

    Initial Commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Robert Morrow on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
